72da639ad82b1ad5e2ff03ebec40618b60612254
Tamari2306 <tamarsolomon23@gmail.com>
2025-05-14 22:08:30 +0300

    updated config.py for testing

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Option 1: Use environment variable (recommended for Render.com)
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
+if os.environ.get("ENV") == "test":
+    DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set.  "
-                     "You must set it as an environment variable.")
+    raise ValueError("DATABASE_URL is not set")
